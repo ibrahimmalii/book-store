4de8d184122a737b4d1a6231e0197187a2d411d6
ibrahimmalii <eng.ibrahimmalii@gmail.com>
2021-10-19 04:23:47 +0200

    handle route for oauth

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -14,6 +14,7 @@ router.post('/register', async (req, res) => {
 
 
 router.post('/login', async (req, res)=>{
+    console.log(req.body)
     try{
         const {email, password} = req.body
         const user = await User.findByCredentials(email, password)
@@ -21,6 +22,26 @@ router.post('/login', async (req, res)=>{
         res.status(200).json({user,token})
     }catch (e){
         res.status(400).json(e)
+    }
+})
+
+
+router.post('/oauthsignup', async(req, res)=>{
+    const {name, email, authToken} = req.body 
+    const ifExist = await User.checkExist(email)
+    console.log(ifExist)
+    if(!ifExist){
+        try{
+            const newUser = await new User({name, email, password : 'randompass'})
+            console.log(newUser)
+            // const password = await newUser.generateRandomPassword()
+            const token = await newUser.generateAuthToken()
+            res.json({user: newUser, token})
+        } catch (e) {
+            res.status(500).json()
+        }
+    }else{
+        res.json({msg:'Email already exist'})
     }
 })
 
