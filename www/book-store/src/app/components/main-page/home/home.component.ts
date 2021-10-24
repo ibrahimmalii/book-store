a5ad3b2ae02933d7f13b35da7c04110a98a10c6b
ibrahimmalii <eng.ibrahimmalii@gmail.com>
2021-10-24 22:20:12 +0200

    remove avatar when execute queries

--- www/book-store/src/app/components/main-page/home/home.component.ts
+++ www/book-store/src/app/components/main-page/home/home.component.ts
@@ -9,17 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-<<<<<<< HEAD
   constructor(private http: HttpClient) { }
 
   users: any;
   books: any;
   responseGet: Boolean = false
-=======
-  constructor() { 
-
-  }
->>>>>>> ed6fef29400b5d78a484a88f990c4dda17237eeb
 
   ngOnInit(): void {
     // this.http.get('http://localhost:8080/api/users').subscribe(res=>{
