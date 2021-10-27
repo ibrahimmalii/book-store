ba3dc2b4658cc9006e11d30c25a309918f23c21e
ibrahimmalii <eng.ibrahimmalii@gmail.com>
2021-10-27 05:48:21 +0200

    handle paypal method routes

--- server/index.js
+++ server/index.js
@@ -6,9 +6,21 @@ const chalk = require('chalk');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv')
 const cors = require('cors')
+const paypal = require('paypal-rest-sdk');
 
 dotenv.config()
 const port = process.env.port || 8080;
+
+// About paypal
+
+paypal.configure({
+  'mode': 'sandbox', //sandbox or live
+  'client_id': process.env.CLIENT_ID,
+  'client_secret': process.env.SECRET_KEY
+});
+
+
+app.get('/', (req, res) => res.sendFile(__dirname + "/index.html"));
 
 
 
@@ -18,6 +30,7 @@ const userRoute = require('./routes/user')
 const bookRoute = require('./routes/book')
 const categoryRouter = require('./routes/category')
 const stripeRouter = require('./routes/stripe')
+const paypalRouter = require('./routes/paypal')
 
 
 
@@ -30,6 +43,7 @@ app.use('/api/users', userRoute)
 app.use('/api/books', bookRoute)
 app.use('/api/categories', categoryRouter)
 app.use('/api/payment', stripeRouter)
+app.use(paypalRouter)
 
 
 
